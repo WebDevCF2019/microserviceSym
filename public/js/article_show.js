bd0fbf80b7700a17f9f65931f8be72cc11192f21
mikhawa <michaeljpitz@gmail.com>
2019-11-15 16:33:35 +0100

    article_show.js

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -1,4 +1,14 @@
 // on utilisera jquery pour notre AJAX (AJAJ)
 $(document).ready(function(){
+    // quand on clique sur le lien dont la classe contient js-like-article, on lance une fonction anonyme, le "e" en paramètre permet de
+    $('.js-like-article').on("click",function (e) {
+        // "e" représente la fonction anonyme, ici on rend le lien non cliquable
 
+        e.preventDefault();
+        // on récupère le lien de la classe
+        let $link = $(e.currentTarget);
+
+        // on change le cœur grâce au css
+        $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
+    });
 });
