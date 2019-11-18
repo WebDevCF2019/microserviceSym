abff71688d595184a806153fdb070e06c0af5798
mikhawa <michaeljpitz@gmail.com>
2019-11-18 09:54:37 +0100

    $.ajax

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -1,14 +1,23 @@
 // on utilisera jquery pour notre AJAX (AJAJ)
 $(document).ready(function(){
-    // quand on clique sur le lien dont la classe contient js-like-article, on lance une fonction anonyme, le "e" en paramètre permet de
+    // quand on clique sur le lien dont la classe contient js-like-article, on lance une fonction anonyme, le "e" en paramètre permet de manipuler celle-ci
     $('.js-like-article').on("click",function (e) {
-        // "e" représente la fonction anonyme, ici on rend le lien non cliquable
 
+        // "e" représente la fonction anonyme, ici on rend le lien non cliquable
         e.preventDefault();
+
         // on récupère le lien de la classe
         let $link = $(e.currentTarget);
 
         // on change le cœur grâce au css
         $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
+
+        // on lance une requête AJAX
+        $.ajax({
+            method:'POST',
+            url: $link.attr('href')
+        }).done(function(donnees){
+            $(".js-like-article-count").html(donnees.heart);
+        });
     });
 });
